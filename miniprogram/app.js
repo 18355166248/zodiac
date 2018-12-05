8a5456c98d471e9fd19e47e9ee7f1709bcef3076
江浪 <643546122@qq.com>
2018-12-05 16:35:26 +0800

    Initial Commit

---
+++ miniprogram/app.js
@@ -0,0 +1,25 @@
+//app.js
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'jiang-fe6b92',
+        traceUser: true,
+      })
+      this.globalData = { isIpx: false }
+      const that = this
+
+      // 获取设备信息
+      wx.getSystemInfo({
+        success(res) {
+          var name = 'iPhone X'
+          if (res.model.indexOf(name) > -1) {
+            that.globalData.isIpx = true
+          }
+        }
+      })
+    }
+  }
+})
---
+++ miniprogram/pages/login/login.js
@@ -0,0 +1,105 @@
+// pages/login/login.js
+var app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      { num: 1 },
+      { num: 2 },
+      { num: 3 },
+      { num: 4 },
+      { num: 5 },
+      { num: 6 },
+      { num: 1 },
+      { num: 2 },
+      { num: 3 },
+      { num: 4 },
+      { num: 5 },
+      { num: 6 }
+    ],
+    btnStyle: '0',
+    animation1: true,
+    bgcPo: '0'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      btnStyle: app.globalData.isIpx ? '120rpx' : '33rpx'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.setTimer()
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    this.clearTimer()
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    this.clearTimer()
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  clearTimer: function () {
+    clearInterval(this.timer)
+    this.timer = null
+  },
+  setTimer: function () {
+    this.changeBg()
+    // 定时器
+    this.timer = setInterval(this.changeBg, 4000)
+  },
+  changeBg: function () {
+    const that = this
+    this.data.list.forEach((v, i) => {
+      let index = i
+      setTimeout(() => {
+        that.setData({
+          bgcPo: - 150 * index + 'rpx'
+        })
+      }, 4000 * (8.3 * index / 100))
+    })
+  }
+})
